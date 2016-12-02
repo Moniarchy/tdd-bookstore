05f0f710d916ef746580c0516b973f3062f22738
Abraham Ferguson <abrahamferguson@Abrahams-MacBook-Pro.local>
2016-12-01 16:49:27 -0800

    success defined as win, with title check, jRobb did this stuff

--- backend/queries.js
+++ backend/queries.js
@@ -8,142 +8,50 @@ const setup = () => {
   const qf = new QueryFile(__dirname+'/schema.sql')
   return database.oneOrNone(qf)
 }
-
 const reset = () => {
-  return database.any(`
-    TRUNCATE books;
-  `)
-}
-const createBook = ( title, year ) => {
-  const sql = `
-    INSERT INTO books( title, year ) VALUES ( $1, $2 ) RETURNING *`
-  const variables = [ title, year ]
-  return database.one( sql, variables )
-  // return Promise.all([
-  //   createAuthor( author )
-  //   associateBookWithAuthor( author )
-  //   .catch( error => { console.log( 'A', error ); throw error }),
-  //
-  //   createGenre( genres )
-  //   .catch( error => { console.log( 'B', error ); throw error }),
-  //
-  //   database.one( sql, variables )
-  //   .catch( error => { console.log( 'C', error  ); throw error })
-  // ])
+  return database.any(`TRUNCATE books;`)
 }
 
-const createAuthor = authorName => {
-  const sql = `
-  INSERT INTO
-  authors (name)
-  VALUES
-  ($1)
-  RETURNING
-  *
-  `
-  return database.one( sql, [authorName] )
+const insertBook = ( title, year ) => {
+  return database.query('INSERT INTO books ( title, year ) VALUES ( $1, $2 ) RETURNING id',[ title, year ]).then(result => result[0].id )
 }
-
+const insertAuthor = (authorName) => {
+  return database.query( 'INSERT INTO authors (name) VALUES ($1) RETURNING id', [authorName]).then(result => result[0].id )
+}
+const insertGenre = ( genre ) => {
+  return database.query( 'INSERT INTO genres ( name ) VALUES ($1) RETURNING id', [genre]).then(result => result[0].id )
+}
 const associateBookWithAuthor = ( book, author ) => {
-  const sql = `
-  INSERT INTO
-  book_authors( book_id, author_id )
-  VALUES
-  ( $1, $2 )
-  RETURNING
-  *
-  `
-  const variables = [book.id, author.id]
-  return database.one( sql, variables )
+  return database.query( 'INSERT INTO book_authors( book_id, author_id ) VALUES ( $1, $2 )', [book, author] )
 }
-const createGenre = ( book, genres ) => {
-  console.log(genres);
-  for( genre in genres ){
-    const sql = `
-    INSERT INTO
-    genres ( name )
-    VALUES
-    ($1)
-    `
-    const variable = [genre]
-    const sqlTwo=`
-    INSERT INTO
-    book_genres ( book_id, genre_id )
-    VALUES
-    ( $1, $2 )`
-    const variables = [ book.id, genre.id ]
-  }
-  return database.manyOrNone( sql, variables )
-}
-
-
-  //   .then( data => {
-  //     deserializeGenres(data[1].name)
-  //     data[1].name = JSON.parse(data[1].name)
-  //     return data
-  // })
-  //   .then( data => {
-  //     // console.log('-------', data);
-  //     // console.log(book)
-  //     return Promise.all([
-  //       associateBookWithAuthor( data[2], data[0] ),
-  //       associateBookWithGenre( data[2], data[1] ),
-  //     ]).then( result => {
-  //       // console.log(result);
-  //       return result} )
-  //   })
-  //   .then( book => {
-  //     getBookById( 2 )
-  //     console.log( '------------', book )
-  //   })
-  //   .catch( error => {console.log( 'E', error ); throw error})
-  //
-
-
-
-// JSON.stringify(genreName.sort())
-
-
 const associateBookWithGenre = ( book, genres ) => {
-  const sql = `
-    INSERT INTO
-      book_genres( book_id, genre_id )
-    VALUES
-      ( $1, $2 )
-    RETURNING
-      *
-  `
-  const variables = [book.id, genres.id, genres.name]
-  return database.one( sql, variables )
+  return database.query( 'INSERT INTO book_genres( book_id, genre_id ) VALUES ( $1, $2 ) ', [book, genres] )
 }
 
-const deserializeGenres = genres => {
-  genres = JSON.stringify(genres)
-  return genres
+const createBook = book => {
+  return Promise.all([
+    insertBook( book.title, book.year ),
+    insertAuthor( book.author ),
+    Promise.all( book.genres.sort().map( genre => insertGenre( genre )) )
+  ]).then( result => {
+      let bookId = result[0]
+      let authorId = result[1]
+      let genresIds = result[2]
+
+      return Promise.all([
+        associateBookWithAuthor(bookId, authorId),
+        ...genresIds.map( genreId => associateBookWithGenre( bookId, genreId ))
+      ])
+  })
+  .catch( error => console.log( 'ERROR', error ))
 }
 
 const getBookById = bookID => {
-  const sql = `
-  SELECT * FROM books
-  WHERE id = ( $1 )
-  `
-
-const variables = [bookID]
-return database.one( sql, variables )
+  return database.query( 'SELECT * FROM books WHERE id = ( $1 )', [bookID] )
 }
 
-
 module.exports = {
-  createAuthor,
   createBook,
-  createGenre,
   setup,
   reset,
 }
-
-
-//sql query that inserts new book into the database
-//sql query that inserts author name into author table
-//sql query that joins the new book's id to an author id in the book_authors TABLE
-//sql query that inserts genre name into genre table
-//sql query that joins the new book's id to an genre id in the book_genres TABLE
--- backend/routes/api/books.js
+++ backend/routes/api/books.js
@@ -6,81 +6,14 @@ const bodyParser = require('body-parser')
 
 //CreateBook
 router.post( '/', ( request, response ) => {
-  const { title, year, author, genres } = request.body
-  // console.log(title, author, year, genres);
-
   // First, create the book
-  db.createBook( title, year )
-  // then find or create the authors
-    .then( book => {
-      db.createAuthor( author )
-      const authors = [] // get these
-        .then(data => {return authors})
+  const { title, author, year, genres } = request.body
 
-      return { book, authors }
-    })
-  // then associate the authors with the book
-    .then( ({ book, authors }) => {
-      db.associateBookWithAuthor( book, authors )
-      return book
-    })
-  // then find or create the genres
-    .then( book => {
-      db.createGenre( book, genres )
-        .then(data => {
-          console.log( '-----', data )
-        })
-      const genres = [] // get these
-
-      return { book, genres }
-    })
-  // then associate the genres with the book
-    // .then( ({ book, genres }) => {
-    //   db.associateBookWithGenre( book, genres )
-    //   return book
-    // })
-  // then return the book as json
-    .then( book => response.status( 201 ).json( book ))
-
-  // console.log(Array.isArray(genres))
-  // db.createGenre( genres )
+  if(request.body.hasOwnProperty(title)){
+    db.createBook( request.body )
+      .then( data => response.status( 201 ).json( data ) )
+  }else{ response.body = { error:{ message: "title cannot be  blank" }}
+    return response.status( 400 ).json( response.body ) }
 })
-// db.createBook( title, year )
-// .then( book => {
-//   console.log('---------', book)
-//   if(book.title) {
-//     response.status(201).json(book)
-//   }else{ response.body = { error:{ message: "title cannot be blank" }}
-//   return response.status( 400 ).json( response.body ) }
-// })
-// .catch( error => {
-//   response.status(500).json({ error })
-// })
-// db.createAuthor( author )
-// router.get('/api/books', (request, response, next) => {
-//   let page = ( parseInt( req.query.page, 10 ) ) || 1
-//   db.getBooks( page )
-//     .then( response.render(//notSure))
-// })
-// router.get('/api/books?author=phILip', (request, response, next) => {
-//   db.getBooksByAuthor(request.body.author)
-//     .then(response.render(//notSure))
-// })
-// router.get('/api/books?title=wORld', (request, response, next) => {
-//   db.getBooksByTitle(request.body.title)
-//     .then(response.render(//notSure))
-// })
-// router.get('/api/books?year=1953', (request, response, next) => {
-//   db.getBooksByYear(request.body.year)
-//     .then(response.render(//notSure))
-// })
-// router.get('/api/books?year=1953&title=th', (request, response, next) => {
-//   db.getBookByTitleAndYear(request.body.year, request.body.title)
-//     .then(response.render(//notSure))
-// })
-// router.put('/api/book/12/delete', (request, response) => {
-//   db.deleteBook(request.body.id)
-//     .then(response.status(200))
-// })
 
 module.exports = router
